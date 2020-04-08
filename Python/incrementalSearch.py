fdb88651d6ac933d2c0c07141c0ae507a98f9cbc
eechava6 <eechava6@eafit.edu.co>
2020-04-08 17:39:26 -0500

    Update: improving incrementalSearch

--- Python/incrementalSearch.py
+++ Python/incrementalSearch.py
@@ -7,37 +7,20 @@ def incrementalSearch (start, step, stop, decimals = 4):
     points = np.arange(start,stop,step)
 
     for point in points:
-        mult = evaluated * f(point)
+        mult = float(evaluated * f(point))
         root = True if evaluated < 0 else False 
-        row = {'f(x)' : round(f(point), decimals),
+        row = {
+            'x': point,
+            'f(x)' : round(f(point), decimals),
             'root': root,
             'f(a)*f(b)':round(mult,decimals),
             'range':[round(point-step,decimals),round(point,decimals)]
             }
         return_list.append(row)
         evaluated = f(point)
-        
 
     return return_list
 
 
 
-def main():
-    print("ingrese start")
-    start = input()
-    print("ingrese stop")
-    stop = input()
-    print("ingrese step")
-    step = input()
-
-    result = incrementalSearch(1,1,10)
-
-    i=0
-    while i < len(result):
-        print(" | "+str(result[i][0])+" | "+str(result[i][1])+" | "+str(result[i][2])+" | "+str(result[i][3])+" | "+str(result[i][4])+" | "+str(result[i][5])+" | ")
-        i += 1
-
-
-if __name__ == "__main__":
-    main()
 
