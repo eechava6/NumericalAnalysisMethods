7007256f72e9be3009cc40920fbae9bf0599c4d9
eechava6 <eechava6@eafit.edu.co>
2020-04-09 22:55:46 -0500

    Update: moved rowOps to function, regressiveSust in function, need to fix xi names in regressive

--- Python/function.py
+++ Python/function.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 def f(x): return (x**4) - (x) - 10
 # pow(x,2)-(3*x)-4
@@ -7,4 +8,37 @@ def df(x): return (4*(x**3))-1
 def ddf(x): return 12*(x**2)
 
 def g(x): return (x+10)**(1/4)
-#(x * math.exp(x) - math.pow(x, 2) - 3) / 5
+
+def regresiveSustitution(Ab,n):
+    solutions = []
+    Ab= np.array(Ab,float)
+    for i in range(n,-1,-1):
+        sum = 0
+        for p in range(i+1,n+1):
+            sum = sum + Ab[i][p] 
+        xi = (Ab[i][n+1] - sum)/Ab[i][i] 
+        solutions.append('x%s = %s' %(i,xi))
+    return solutions
+
+def getArr(n):
+    arr = []
+    for val in range(0,n+1):
+        arr.append(val)
+    return arr
+
+def rowOps(Ab,nCol):
+    
+    col = Ab[:,nCol]
+    f = lambda x: x/col[nCol]
+    multipliers = f(col)
+    multipliers = multipliers[nCol+1:]
+    #Verify if worth doing the op (Multipliers different to zero)
+    if(np.count_nonzero(multipliers) == 0):
+        return 0
+    pivot = np.tile(Ab[nCol,:],(col.size-nCol-1,1))
+    mMatrix = (pivot.transpose()*multipliers).transpose()
+    arr = getArr(nCol)
+    r = np.delete(Ab,arr,0) - mMatrix
+    for val in reversed(arr):
+        r = np.insert(r,0,[Ab[val,:]],0)
+    return r
--- Python/gaussPartialPivot.py
+++ Python/gaussPartialPivot.py
@@ -1,41 +1,26 @@
 
 import numpy as np
 import pandas as pd
-
-
-def swapMaxAbs(Ab,nCol):
-    absCol = np.absolute(Ab[nCol:,nCol])
-    mVal = np.argmax(absCol)
-    Ab[[nCol, mVal+nCol]] = Ab[[mVal+nCol , nCol]]
-    return Ab
-
-def rowOps(Ab,nCol):
-    col = Ab[:,nCol]
-    f = lambda x: x/col[nCol]
-    multipliers = f(col)
-    multipliers = multipliers[nCol+1:]
-    pivot = np.tile(Ab[nCol,:],(col.size-nCol-1,1))
-    mMatrix = (pivot.transpose()*multipliers).transpose()
-    arr = getArr(nCol)
-    r = np.delete(Ab,arr,0) - mMatrix
-    for val in reversed(arr):
-        r = np.insert(r,0,[Ab[val,:]],0)
-    return r
-
-def getArr(n):
-    arr = []
-    for val in range(0,n+1):
-        arr.append(val)
-    return arr
+from function import regresiveSustitution
+from function import rowOps
 
 def partialPivot(A):
     pivots = []
     A = np.array(A).astype(float)
     pivots.append(A)
     times = A[:,0].size-1
-    for index in range(0,times):
-        maxA = swapMaxAbs(A,index)
-        pivots.append(maxA)
-        A = rowOps(maxA,index)
-        pivots.append(A)
+    for nCol in range(0,times):
+        absCol = np.absolute(A[nCol:,nCol])
+        mVal = np.argmax(absCol)
+        if(A[nCol][nCol] < mVal):
+            A[[nCol, mVal+nCol]] = A[[mVal+nCol , nCol]]
+            pivots.append(A)
+        #rowOps validates if it worth, if not returns 0, else returns 
+        #switched rows in matrix
+        ops = rowOps(A,nCol)
+        if(not isinstance(ops,int)):
+            A = ops 
+            pivots.append(A)
+    values = regresiveSustitution(pivots[-1],times)
+    pivots.append(values)
     return pivots
